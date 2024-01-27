ee13aee5d869ccf92acf17dcb4c02dcc8c31e00a
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2024-01-27 12:02:23 -0500

    Landing page completed

--- front-end/src/pages/Landing.js
+++ front-end/src/pages/Landing.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import linebreak from "./rustic-line-break.png";
 
 export const Landing = () => {
     const navigate = useNavigate();
@@ -8,14 +9,18 @@ export const Landing = () => {
     }
     return (
         <div className="h-screen w-screen flex flex-col bg-[#F7F2EC] items-center justify-center">
-            <h1 id="cinzel" className="text-6xl text-[#6A482F]">MEMORIFY</h1>
-            <h2  className="text-xl max-w-3xl">
+            <h1 id="cinzel" className="text-6xl text-[#6A482F] drop-shadow-lg mb-4">MEMORIFY</h1>
+            <div className="w-1/2 h-10 mb-3">
+                <img src={linebreak} alt="Line Break" className="object-fill w-full h-full"/>
+            </div>
+    
+            <h2 id="playfair" className="text-xl max-w-3xl font-medium mb-12">
                 Capture your cherished moments with our innovative photo album experience 
                 and explore the magic of reliving your memories in a unique and engaging way!
             </h2>
-            <div className="flex space-x-16 text-xl">
-                <button>LOG IN</button>
-                <button>SIGN UP</button>
+            <div className="flex space-x-16 text-lg text-[#DDD0C8]">
+                <button id="button-shadow" className="bg-[#61544C] py-2 px-14 font-medium">LOG IN</button>
+                <button id="button-shadow" className="bg-[#61544C] py-2 px-12 font-medium">SIGN UP</button>
             </div>
         </div>
     
