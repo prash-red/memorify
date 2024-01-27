5f782513aec41a7645bb55727283bc8475d0660c
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2024-01-27 11:07:05 -0500

    Added cinzel & playfair fonts

--- front-end/src/pages/Landing.js
+++ front-end/src/pages/Landing.js
@@ -8,8 +8,8 @@ export const Landing = () => {
     }
     return (
         <div className="h-screen w-screen flex flex-col bg-[#F7F2EC] items-center justify-center">
-            <h1 className="text-6xl text-[#6A482F]">MEMORIFY</h1>
-            <h2 className="text-xl max-w-3xl">
+            <h1 id="cinzel" className="text-6xl text-[#6A482F]">MEMORIFY</h1>
+            <h2  className="text-xl max-w-3xl">
                 Capture your cherished moments with our innovative photo album experience 
                 and explore the magic of reliving your memories in a unique and engaging way!
             </h2>
