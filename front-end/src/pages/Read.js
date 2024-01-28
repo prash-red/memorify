d3000d761d925596a2cea421d4257feb130acc95
04kash <m04kashish@gmail.com>
2024-01-28 06:45:49 -0500

    modified album and read

--- front-end/src/pages/Read.js
+++ front-end/src/pages/Read.js
@@ -4,19 +4,18 @@ import back from "./assets/turn-back.png"
 import lamp from "./assets/lamp.png"
 import oldparchment from "./assets/old-parchment.png"
 
-export const Read = () => {
+export const Read = (props) => {
 
     const navigate = useNavigate();
-
     return (
         <div className="h-screen w-screen overflow-y-hidden" style={{ backgroundImage: `url(${oldparchment})`}}>
             <button onClick={() => navigate("/album")}>
-                <img src={back} alt="back" className="absolute top-[20vh] left-[30vw] h-[5vh]" />
+                <img src={back} alt="back" className="absolute top-[20vh] left-[35vw] h-[5vh]" />
             </button>
             <div className="flex">
-                <h3 className="absolute top-[21vh] left-[35vw]" >Untitled Entry</h3>
-                <h3 className="absolute top-[21vh] left-[60vw]" >Timestamp</h3>
-                <h3 className="absolute top-[28vh] left-[35vw]" >Content Goes Here...</h3>
+                <h3 className="absolute top-[21vh] left-[47vw]" >{props.title}</h3>
+                {/*<h3 className="absolute top-[21vh] left-[60vw]" >{props.date}</h3>*/}
+                <h3 className="absolute top-[28vh] left-[33vw] h-[80vh] w-[35vw]" >{props.content}</h3>
                 <div>
                     <img src={entry} alt="journal entry" className="h-[98vh] w-[55vw] ml-[23vw]" />
                 </div>
