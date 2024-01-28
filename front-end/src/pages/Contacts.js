450b6ed321b31f9c66d2ebe6b2cb9d75f9824bf7
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2024-01-27 23:22:30 -0500

    Contact list

--- front-end/src/pages/Contacts.js
+++ front-end/src/pages/Contacts.js
@@ -3,12 +3,49 @@ import marvel from "./assets/marvel.png"
 import looney from "./assets/looney.png"
 import newparc from "./assets/new-parchment.png"
 import oldparchment from "./assets/old-parchment.png";
+import list from "./assets/contact-list.png";
+
 export const Contacts = () =>{
     return (
         <>
             <div className="flex flex-row h-screen w-screen" style={{ backgroundImage: `url(${oldparchment})` }}>
-                <div className="w-[50vw]">
+                <div className="w-[50vw] flex flex-col items-center justify-center">
                     <h1 id="cinzel" className="text-4xl text-[#6A482F] drop-shadow-lg m-5">Enter Contact</h1>
+                    <div className="h-5/6 w-9/12 bg-contain bg-no-repeat flex flex-col overflow-y-auto px-12 pt-20 pb-96" style={{ backgroundImage: `url(${list})` }}>
+
+                        <div className="mb-4 flex w-full items-center justify-between">
+                            <label id="playfair" className="text-[#6A482F] text-2xl">
+                                NAME
+                            </label>
+                            <input id="cinzel-light" type="text"className="ml-9 text-2xl bg-transparent border-b-2 border-[#855048] outline-none"/>
+                        </div>
+
+                        <div className="mb-4 flex w-full items-center justify-between">
+                            <label id="playfair" className="text-[#6A482F] text-2xl">
+                                NAME
+                            </label>
+                            <input id="cinzel-light" type="text"className="ml-9 text-2xl bg-transparent border-b-2 border-[#855048] outline-none"/>
+                        </div>
+                        
+                        <div className="mb-4 flex w-full items-center justify-between">
+                            <label id="playfair" className="text-[#6A482F] text-2xl">
+                                NAME
+                            </label>
+                            <input id="cinzel-light" type="text"className="ml-9 text-2xl bg-transparent border-b-2 border-[#855048] outline-none"/>
+                        </div>
+
+                        <div className="mb-4 flex w-full items-center justify-between">
+                            <label id="playfair" className="text-[#6A482F] text-2xl">
+                                NAME
+                            </label>
+                            <input id="cinzel-light" type="text"className="ml-9 text-2xl bg-transparent border-b-2 border-[#855048] outline-none"/>
+                        </div>
+
+
+                        
+
+                    </div>
+                    {/* <img src={list} alt="Contacts List" className="h-5/6" /> */}
                 </div>
                 <div className="w-[50vw]">
                     <h1 id="cinzel" className="text-4xl text-[#6A482F] drop-shadow-lg m-5">Select an art style</h1>
