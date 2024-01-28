409be0b65fbc465ef10c8843b400bdad217fe57e
04kash <m04kashish@gmail.com>
2024-01-27 21:22:48 -0500

    added Read.js

--- front-end/src/pages/Read.js
+++ front-end/src/pages/Read.js
@@ -1,23 +1,27 @@
 import entry from "./assets/journal_entry.png";
 import back from "./assets/turn-back.png"
-import viewEntry from "./assets/read.png";
 import lamp from "./assets/lamp.png"
+import oldparchment from "./assets/old-parchment.png"
+import view from "./assets/view-entry.png"
+
 export const Read = () => {
-    return (<div className="relative bg-[#69472E]">
-        <button><img src={back}  alt="back" className="absolute top-[20vh] left-[15vw] h-[5vh]"/></button>
-       <div className="flex flex-col">
-        {/*<h3*/}
-        {/*    className="w-[15vw] ml-4 p-2 absolute top-[20vh] left-[15vw] bg-transparent outline-none"*/}
-        {/*>Untitled Entry</h3>*/}
-        {/*<h3 className="w-[15vw] ml-4 p-2 absolute top-[20vh] left-[65vw] bg-transparent text-right outline-none"*/}
-        {/*> DATE</h3>*/}
-        {/*<h3>Content goes here</h3>*/}
-           <div>
-        <img src={entry} alt="journal entry" className="h-[97vh] w-[55vw]" />
-           </div>
-           <div>
-        <img src={lamp} alt="lamp" />
-           </div>
-       </div>
-    </div>);
+    return (
+        <div className="h-screen w-screen overflow-y-hidden" style={{ backgroundImage: `url(${oldparchment})`}}>
+            <button>
+                <img src={back} alt="back" className="absolute top-[20vh] left-[30vw] h-[5vh]" />
+            </button>
+            <div className="flex">
+                <h3 className="absolute top-[21vh] left-[35vw]" >Untitled Entry</h3>
+                <h3 className="absolute top-[21vh] left-[60vw]" >Timestamp</h3>
+                <h3 className="absolute top-[28vh] left-[35vw]" >Content Goes Here...</h3>
+                <div>
+                    <img src={entry} alt="journal entry" className="h-[98vh] w-[55vw] ml-[23vw]" />
+                </div>
+                <div>
+                    <img src={lamp} alt="lamp" className="mt-[33vh] mr-[15vw] h-[70vh]"/>
+                </div>
+            </div>
+        </div>
+    );
+
 }
